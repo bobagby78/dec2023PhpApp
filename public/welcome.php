3e760d33c9df7e8a130f43d1789b2c515d926be6
bobagby78 <bobagby78@gmail.com>
2024-02-11 07:12:01 -0600

    restructure with public folder

---
+++ public/welcome.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+
+// echo ('Hello, ' . $_SESSION['username']);
+$activeUser = ucfirst($_SESSION['username']); //ucfirst = upper case first of passed string.
+$userid = $_SESSION['userid'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/assets/styles.css">
+    <link rel="icon" href="/public/assets/nerd.jpg">
+    <title>Welcome</title>
+</head>
+<body>
+    <div class="div-block">
+        <h1>Welcome, <?=$activeUser?></h1>
+        <h2> Your user id is <?=$userid?></h2>
+        <p>what would you like to do today?</p>
+        <p>Go to my <a href='/public/resources.php?=<?=$userid?>'>resources</a></p>
+        <p><a href='/public/logout.php'>Sign out</a></p>
+
+    </div>
+    
+</body>
+</html>
+
+
