9f68e10e41fb6e0703b0b11501659d660ed67070
bobagby78 <bobagby78@gmail.com>
2024-05-27 09:50:22 -0500

    clean up db config

--- cfg.php
+++ cfg.php
@@ -2,27 +2,8 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// Load environment variables from .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
-// $host = 'localhost'; //set the host
-// $port = "5432";
-// $dbname = 'dec2023phpapp'; //set the db name
-// $user = 'postgres'; //db user
-// $password = 'postgres'; //db password
-// // $connxnStr = "host={$host} port={$port} dbname={$dbname} password={$password}";
-// // $dbConn = pg_connect($connxnStr);
-
-// $db = new PDO("pgsql:host=$host;port=$port; dbname=$dbname; user=$user; password=$password"); //PDO = PHP Data Objects, this is how php will gain access to the DB
-// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //
-
-// if (!$dbConn){
-//     // echo 'dbConn failed' . pg_last_error();
-//     echo 'dbConn failed';
-// }else{
-//     echo 'here we go';
-// }
 
 $host = $_ENV['DB_HOST'];
 $port = $_ENV['DB_PORT'];
@@ -36,11 +17,7 @@ $connxn = pg_connect($connxnString);
 if (!$connxn) {
     die("Connection failed: " . pg_last_error());
     error_log("$connxn failed");
-}else{
+} else {
 
     error_log("connxn sucsfl");
 }
-
-?>
-
-
